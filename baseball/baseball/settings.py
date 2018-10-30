94bed05a0e7618960681ce73636cb85ab45bdb08
nnashiki <n.nashiki.work@gmail.com>
2018-10-31 01:35:11 +0900

    優先順位をつける

--- baseball/baseball/settings.py
+++ baseball/baseball/settings.py
@@ -9,8 +9,6 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
-# 自作 pipeline に繋げる
-ITEM_PIPELINES = {'baseball.pipelines.MyImagesPipeline': 1}
 # データの保存場所
 IMAGES_STORE = 'images'
 # リンクを辿る深さを指定
